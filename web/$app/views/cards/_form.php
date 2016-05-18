5f279d34a8bba20664621722a3044ece53cc1a62
borodobor <borodobor@gmail.com>
2016-05-18 19:30:51 +0300

    Vendors

---
+++ web/$app/views/cards/_form.php
@@ -0,0 +1,33 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\Cards */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="cards-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'series')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'number')->textInput() ?>
+
+    <?= $form->field($model, 'create_date')->textInput() ?>
+
+    <?= $form->field($model, 'expiration_date')->textInput() ?>
+
+    <?= $form->field($model, 'amount')->textInput() ?>
+
+    <?= $form->field($model, 'status')->textInput() ?>
+
+    <div class="form-group">
+        <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
